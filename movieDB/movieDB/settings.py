c098cf8d2068408f10ee6874bedaae5adbc78291
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-05-09 11:22:59 -0400

    configuring settings.py to use a mysql database

--- movieDB/movieDB/settings.py
+++ movieDB/movieDB/settings.py
@@ -57,9 +57,18 @@ WSGI_APPLICATION = 'movieDB.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default2': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
