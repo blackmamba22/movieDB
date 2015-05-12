e9e0ec2d598c112fb683830189f491621d96f683
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-05-12 19:07:55 -0400

    updated url for movie application and created a basic index view.

---
+++ movieDB/movie/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+
+import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.hello, name="index"),
+
+)
--- movieDB/movie/views.py
+++ movieDB/movie/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+    import datetime
+
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
