ab33955d05ecf097289e9aa6cee28c59ca59ccfe
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-05-14 22:10:13 -0400

    added static folder with twitter boostrap and updated settings.py

--- movieDB/movieDB/settings.py
+++ movieDB/movieDB/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.101']
 
 # Template directories
 
@@ -94,4 +94,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+# Media
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
