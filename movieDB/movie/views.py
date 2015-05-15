a005c558ee5ee2968f0a42416b8c911e9435dc51
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-05-14 22:13:25 -0400

    added base template and index page. updated view for index to start the frontend portion. updated urls for movie app. just skeleton work, needs more updates.

--- movieDB/movie/views.py
+++ movieDB/movie/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Movie
 
 # Create your views here.
 
 def hello(request):
     import datetime
 
-    now = datetime.datetime.now()
+    now = str(datetime.datetime.now())
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+def index(request):
+    return render(request, 'movie/index.html', {})
