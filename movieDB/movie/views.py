32a99daf51338593f0cb9b796d4803ff31a30678
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-05-17 19:16:44 -0400

    updated frontend portion to show movies and tv shows from database when matches are found. updated index page and corresponding index view.

--- movieDB/movie/views.py
+++ movieDB/movie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from models import Movie
+from forms import MovieSearchForm
 
 # Create your views here.
 
@@ -11,5 +12,23 @@ def hello(request):
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
 
-def index(request):
+def index3(request):
+    print request.GET
     return render(request, 'movie/index.html', {})
+
+
+def index(request):
+
+    if 'moviequery' in request.GET and request.GET['moviequery']:
+        moviequery = request.GET['moviequery']
+
+        movies = Movie.objects.filter(title__icontains=moviequery)
+        context_dict = {'movies' : movies}
+
+        return render(request, 'movie/index.html', context_dict)
+    else:
+        print request.POST
+        return render(request, 'movie/index.html', {})
+
+
+
