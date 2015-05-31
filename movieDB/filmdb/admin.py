edaa5bc61d2e2ccbb3d8d0353d8029e70acf67c7
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-05-31 18:49:13 -0400

    updated admin site to show the major models in the filmdb application.

--- movieDB/filmdb/admin.py
+++ movieDB/filmdb/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from models import Genre, Film, Actor
 
 # Register your models here.
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'gender', 'age')
+    search_fields = ('full_name',)
+
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('title', 'released', 'type', 'runtime','imdb_rating')
+    search_fields = ('title',)
+    list_filter = ('year',)
+    ordering = ('title',)
+
+admin.site.register(Film, FilmAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Actor, ActorAdmin)
