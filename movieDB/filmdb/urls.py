7e172343ffff11fe51db21af1f816516d05c465c
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-05-31 15:12:14 -0400

    created a urls.py for filmdb application

--- movieDB/filmdb/urls.py
+++ movieDB/filmdb/urls.py
@@ -1,1 +1,11 @@
-__author__ = 'gawaine'
+from django.conf.urls import patterns, include, url
+
+import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^', views.index, name="index"),
+
+)
