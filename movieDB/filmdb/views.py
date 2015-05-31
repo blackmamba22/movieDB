cc23726a05854836b67a9eb420ecaaf36775a576
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-05-31 15:14:49 -0400

    created a basic index view

--- movieDB/filmdb/views.py
+++ movieDB/filmdb/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    import datetime
+
+    now = str(datetime.datetime.now())
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
